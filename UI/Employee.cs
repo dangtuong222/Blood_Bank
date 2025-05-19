658f4dcc2881d93797c44ab3aef5a550d95868ca
quanbao2603 <nguyenquanbao2632005@gmail.com>
2025-05-19 18:32:54 +0700

    19/5

---
+++ UI/Employee.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blood_Bank.UI
+{
+    public partial class Employee : Form
+    {
+        public Employee()
+        {
+            InitializeComponent();
+            populate();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=BloodBankDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;MultiSubnetFailover=False");
+        
+        private void reset()
+        {
+            EmpNameTb.Text = "";
+            EmpPassTb.Text = "";
+        }
+
+        private void populate()
+        {
+            con.Open();
+            string query = "select * from EmployeeTbl";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            SqlCommandBuilder builder = new SqlCommandBuilder(sda);
+            var ds = new DataSet();
+            sda.Fill(ds);
+            EmpDGV.DataSource = ds.Tables[0];
+            con.Close();
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            if(EmpNameTb.Text =="" || EmpPassTb.Text == "") 
+            {
+                MessageBox.Show("Missing Information");
+            }
+            else
+            {
+                try
+                {
+                    string query = "insert into EmployeeTbl (EmpId, EmpPass) values('" + EmpNameTb.Text +"','"+ EmpPassTb.Text+"')";
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.ExecuteNonQuery();  
+                    MessageBox.Show("Employee Successfully Saved");
+                    con.Close();
+                    reset();
+                }   
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            Login log = new Login();
+            log.Show();
+            this.Hide();
+        }
+    }
+}
